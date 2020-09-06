c395db13df4915c92fe5c88ca38fd65e7f755e9e
wang90 <393210564@qq.com>
2020-09-06 16:42:21 +0800

    ci: add mine page

---
+++ src/pages/mine/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <view class="mine">
+    关于我
+  </view>
+</template>
+<style lang="less">
+@import "./index.less";
+</style>
+<script>
+
+export default {
+  name:"Mine",
+  data () {
+    return {}
+  },
+}
+</script>
